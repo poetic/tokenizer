d4233f141b827c95aef7ec06dd97658c86434e5d
Leonidez Acosta <leonidez@poeticsystems.com>
2015-06-05 14:41:13 -0500

    initial commit

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'poetic:tokenizer',
+  version: '0.0.1',
+  summary: 'create tokens to validate user interactions',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('accounts-base', ['client', 'server']);
+  api.use('momentjs:moment', ['client', 'server']);
+
+  api.addFiles('tokenizer.js', ['client', 'server']);
+  api.export('Tokenizer');
+});
+
+Package.onTest(function(api){});
