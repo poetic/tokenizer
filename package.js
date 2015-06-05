33b7685c79bedc4e922c3a10044ad415976f3cd2
Leonidez Acosta <leonidez@poeticsystems.com>
2015-06-05 16:40:21 -0500

    tokenizer.verify method

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function(api){
   api.versionsFrom('1.1.0.2');
 
   api.use('accounts-base', ['client', 'server']);
--- tokenizer.js
+++ tokenizer.js
@@ -15,7 +15,51 @@ Tokenizer = {
     return tokenRecord.token;
   },
 
-  verify: function(){
+  verify: function(token, callback){
+    Meteor.call('verifyToken', token, function(result){
+      if (result.error) { return callback(result.error) }
 
+      Accounts.verifyEmail(token, function(e){
+        if (e) {
+          callback(new Error('token is not valid'));
+        } else {
+          callback(null, true);
+        }
+      });
+    });
   }
 };
+
+Meteor.methods({
+  verifyToken: function(token){
+    var result = {};
+    var user;
+    var tokenRecord;
+
+    if (Meteor.isServer) {
+      user = Meteor.users.findOne({
+        'services.email.verificationTokens.token': token
+      });
+
+      if (user) {
+        tokenRecord = _.find(user.services.email.verificationTokens, function(tokenObj){
+          return tokenObj.token = token;
+        });
+
+        if (tokenRecord.expires) {
+          var interval = Object.keys(tokenRecord.expires)[0];
+          var quantity = tokenRecord.expires[interval];
+
+          var expiration = moment(tokenRecord.when).add(quantity, interval);
+          var now = moment();
+
+          if (now.isBefore(expiration)){
+            return true;
+          } else {
+            return { error: 'token has expired' };
+          }
+        }
+      }
+    }
+  }
+});
